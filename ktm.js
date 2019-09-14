85047209c506a9729fd5bc594aed2e7cd448b5d6
Soo Hian <shonendumm@gmail.com>
2019-09-14 14:35:52 +0800

    Errors in scraping results. will fix later.

--- ktm.js
+++ ktm.js
@@ -2,19 +2,18 @@ const puppeteer = require('puppeteer');
 
 
 const dateGo = '12/10/2019'; // date of going JB must be xx/xx/xx
-const dateGoArray = ['12/10/2019','06/10/2019','07/10/2019']; // list of potential dates to go
-
-const dateReturn = dateGo; // date of coming back
-const dateReturnArray = dateGoArray; // list of dates coming back. Same since day trip
+// const dateGoArray = ['12/10/2019','06/10/2019','07/10/2019']; // list of potential dates to go
+const dateGoArray = ['28/09/2019', '01/10/2019', '11/10/19','12/10/19']
+const dateReturnArray = ['29/09/2019']; // list of dates coming back. 
 
 const timeGo = 1; // 1: all timings, 2: 12am-7am, 3: 7am-12pm, 4: 12pm-7pm, 5: 7pm-12am
-const timeReturn = 1; // not sure if correct TODO:
+const timeReturn = 4; // as above.
 
 let repeat = true; // for implementing user input later, maybe
 
 // comment out function depending on going or coming back. Can run both too. Not advisable though
-start();
-// back();
+// start();
+back();
 
 // should ask for user input to enter list of dates TODO:
 
@@ -26,7 +25,7 @@ async function start() {
 
         const page = await browser.newPage();
         await page.goto('https://eticket.ktmb.com.my/eticket');
-        await page.setViewport({width: 1200, height:1200});
+        await page.setViewport({width: 1200, height:800});
 
         await page.waitFor(1000);
         
@@ -51,7 +50,7 @@ async function start() {
         }
         await page.keyboard.press('Escape');
 
-        await page.waitFor(1000); // check time
+        // await page.waitFor(1000); // check time
 
         await page.click('#taboneway > form > ul:nth-child(3) > li:nth-child(1) > select');
         await page.keyboard.type('2'); // 2 adults
@@ -59,19 +58,25 @@ async function start() {
 
         let picName = date.replace('/', '-'); // date string contains '/' which interferes with saving pic
         picName = picName.replace('/', '-'); // do it twice since can't no replaceAll function and using escape // is a comment
-        await page.screenshot({ fullPage:true, path: `${picName}.png` }); // take a pic before sending request
+        await page.screenshot({ fullPage:true, path: `${picName}_going.png` }); // take a pic before sending request
         await page.click('#taboneway > form > ul:nth-child(3) > li:nth-child(3) > input');
 
-        await page.waitFor(5000);      
+        // await page.waitFor(5000);      
         const result = await page.evaluate(() => { // check train timing results
-            let initialRes = document.querySelector('#\\31  > div:nth-child(3) > div:nth-child(1) > div > span.skin-clr');            
-            if (initialRes == null) {
-                initialRes = document.querySelector('#tab4 > div:nth-child(4)');
-            }    
-            let text = initialRes.innerText;        
+            let text = 'train';
+            text = document.querySelector('#\\31  > div:nth-child(3) > div:nth-child(1) > div > span.skin-clr').innerText;
+            // if (document.querySelector('#\31  > div:nth-child(3) > div:nth-child(1) > div > span.skin-clr') != null) { // check for departure trains
+            //     // console.log(text);            
+            // } 
+            if (document.querySelector('#tab4 > div:nth-child(4)') != null) { // check for no trains
+                text = document.querySelector('#tab4 > div:nth-child(4)').innerText;
+            } 
+            if (document.querySelector('#tab4 > div:nth-child(3)') != null) { // check for trains fully booked
+                text = document.querySelector('#tab4 > div:nth-child(3)').innerText;
+            }
             return text;
         });
-        // console.log(result);
+        console.log(result);
         return result;
         
     };
@@ -79,7 +84,7 @@ async function start() {
     for (var i = 0; i < dateGoArray.length; i++) { // iterate through dates provided
         let x = 0; // reinitialise it everytime.
         x = await goKTM(dateGoArray[i]);
-        if (x.includes('Opps')) {
+        if ( x == null || x.includes('sorry')) {
             console.log(`No going train for ${dateGoArray[i]}`);
         } else { // else if x.includes(positive results)
         console.log(`Success! Got going train for ${dateGoArray[i]}, go book it`);
@@ -109,7 +114,7 @@ async function back() {
 
         const page = await browser.newPage();
         await page.goto('https://eticket.ktmb.com.my/eticket');
-        await page.setViewport({width: 1200, height:1200});
+        await page.setViewport({width: 1200, height:800});
 
         await page.waitFor(1000);
         
@@ -136,7 +141,7 @@ async function back() {
         }
         await page.keyboard.press('Escape');
 
-        await page.waitFor(1000); // for user to do visual check time
+        // await page.waitFor(1000); // for user to do visual check time
 
         await page.click('#taboneway > form > ul:nth-child(3) > li:nth-child(1) > select');
         await page.keyboard.type('2'); // 2 adults
@@ -144,20 +149,15 @@ async function back() {
 
         let picName = date.replace('/', '-'); // date string contains '/' which interferes with saving pic
         picName = picName.replace('/', '-'); // do it twice since can't no replaceAll function and using escape // is a comment
-        await page.screenshot({ fullPage:true, path: `${picName}.png` }); // take a pic before sending request
+        await page.screenshot({ fullPage:true, path: `${picName}_return.png` }); // take a pic before sending request
         await page.click('#taboneway > form > ul:nth-child(3) > li:nth-child(3) > input');
 
-        await page.waitFor(5000);      
+        await page.waitFor(6000);      
         const result = await page.evaluate(() => { // check train timing results
-            
-            let initialRes = document.querySelector('#\\31  > div:nth-child(3) > div:nth-child(1) > div > span.skin-clr');            
-            if (initialRes == null) {
-                initialRes = document.querySelector('#tab4 > div:nth-child(4)');
-            }    
-            let text = initialRes.innerText;        
-            return text;
+            let initialResult = document.querySelector('#tab4 > div:nth-child(3)').innerText; // check for fully booked trains
+            console.log(initialResult);        
+            return initialResult;
         });
-        // console.log(result);
         return result;
         
     };
@@ -165,12 +165,12 @@ async function back() {
     for (var i = 0; i < dateReturnArray.length; i++) { // iterate through dates provided
         let x = 0; // reinitialise it everytime.
         x = await returnKTM(dateReturnArray[i]);
-        if (x.includes('Opps')) {
-            console.log(`No return train for ${dateReturnArray[i]}, pls try again.`);
-        } else { // else if x.includes(positive results)
-        console.log(`Success! Got return train for ${dateReturnArray[i]}, go book it`);
-        repeat = false; 
-        };
+        // if (x.includes('sorry')) {
+        //     console.log(`No return train for ${dateReturnArray[i]}, pls try again.`);
+        // } else { // else if x.includes(positive results)
+        //     console.log(`Success! Got return train for ${dateReturnArray[i]}, go book it`);
+        // repeat = false; 
+        // };
     };
 }
 
