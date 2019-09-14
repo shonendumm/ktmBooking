3c42a5b4a1c637a85419ed1c036f45ed719bcf06
Soo Hian <shonendumm@gmail.com>
2019-09-14 11:57:29 +0800

    Revise returning train function for timings

--- ktm.js
+++ ktm.js
@@ -4,22 +4,26 @@ const puppeteer = require('puppeteer');
 const dateGo = '12/10/2019'; // date of going JB must be xx/xx/xx
 const dateGoArray = ['06/10/2019','07/10/2019']; // list of potential dates to go
 
-const dateReturn = dateGo; // date of coming back 
-const timeGo = 1; // 1 = all, 2 = 12am-7am, 3 = 7am-12pm, 4 = 12pm-7pm, 5 = 7pm-12am
+const dateReturn = dateGo; // date of coming back
+const dateReturnArray = dateGoArray; // list of dates coming back
+
+const timeGo = 1; // 1: all timings, 2: 12am-7am, 3: 7am-12pm, 4: 12pm-7pm, 5: 7pm-12am
 const timeReturn = 1; // not sure if correct TODO:
-let repeat = true;
 
-start();
+let repeat = true; // for implementing user input later, maybe
 
+// comment out function depending on going or coming back. Can run both too.
+// start();
+back();
 
-// returnKTM();
+// should ask for user input to enter list of dates TODO:
+
 async function start() {
-    const browser = await puppeteer.launch({headless:false});
     
-    let goKTM = async (date) => {
-        
-        // should ask for user input to continue? If no, repeat = false
-        // ask user input for date and time
+    const browser = await puppeteer.launch({headless:false});
+    // let goKTM = async (date) => {
+    async function goKTM(date) {
+
         const page = await browser.newPage();
         await page.goto('https://eticket.ktmb.com.my/eticket');
         await page.setViewport({width: 1200, height:1200});
@@ -59,11 +63,7 @@ async function start() {
         await page.click('#taboneway > form > ul:nth-child(3) > li:nth-child(3) > input');
 
         await page.waitFor(5000);      
-        const result = await page.evaluate(() => {
-            // #\33  > div:nth-child(3) > div:nth-child(1) > div > span.skin-clr
-            // #\32  > div:nth-child(3) > div:nth-child(1) > div > span.skin-clr
-            // #\31  > div:nth-child(3) > div:nth-child(1) > div > span.skin-clr
-
+        const result = await page.evaluate(() => { // check train timing results
             let initialRes = document.querySelector('#\\31  > div:nth-child(3) > div:nth-child(1) > div > span.skin-clr');            
             if (initialRes == null) {
                 initialRes = document.querySelector('#tab4 > div:nth-child(4)');
@@ -77,7 +77,7 @@ async function start() {
     };
 
     for (var i = 0; i < dateGoArray.length; i++) { // iterate through dates provided
-        var x = 0; // reinitialise it everytime.
+        let x = 0; // reinitialise it everytime.
         x = await goKTM(dateGoArray[i]);
         if (x.includes('Opps')) {
             console.log(`No train for ${dateGoArray[i]}, pls try again.`);
@@ -101,56 +101,77 @@ async function start() {
 };
 
 
-
-async function returnKTM() {
-    const browser = await puppeteer.launch({headless:false});
-    const page = await browser.newPage();
-    await page.goto('https://eticket.ktmb.com.my/eticket');
-    await page.setViewport({width: 1000, height:800});
-
-    await page.waitFor(1000);
+async function back() {
     
-    await page.click('#taboneway > form > ul:nth-child(1) > li:nth-child(1) > select');
-    // await page.keyboard.type('O') // origin
-    await page.keyboard.type('J'); // JB
-    await page.keyboard.press('Escape');
+    const browser = await puppeteer.launch({headless:false});
+    // let goKTM = async (date) => {
+    async function returnKTM(date) {
 
-    await page.waitFor(1000); // wait for UI after selection, so can choose Woodlands
+        const page = await browser.newPage();
+        await page.goto('https://eticket.ktmb.com.my/eticket');
+        await page.setViewport({width: 1200, height:1200});
 
+        await page.waitFor(1000);
+        
+        await page.click('#taboneway > form > ul:nth-child(1) > li:nth-child(1) > select'); // select departure
+        await page.keyboard.type('J'); // JB
+        await page.keyboard.press('Escape');
 
-    await page.click('#taboneway > form > ul:nth-child(1) > li:nth-child(2) > select');
-    await page.keyboard.type('ww'); // woodlands
-    await page.keyboard.press('Escape');
+        await page.waitFor(1000); // wait for UI after selection
 
-    await page.waitFor(1000); // wait for UI after selection
+        await page.click('#taboneway > form > ul:nth-child(1) > li:nth-child(2) > select'); // select destination
+        await page.keyboard.type('ww'); // woodlands
+        await page.keyboard.press('Escape');
 
-    await page.click('#taboneway > form > ul:nth-child(1) > li:nth-child(3) > div > input');
-    await page.click('#taboneway > form > ul:nth-child(1) > li:nth-child(3) > div > div > ul > li.uib-button-bar.ng-scope > span > button.btn.btn-sm.btn-danger.uib-clear.ng-binding');
-    await page.keyboard.type(dateReturn); 
+        await page.waitFor(1000); // wait for UI after selection
 
-    await page.click('#taboneway > form > ul:nth-child(1) > li:nth-child(4) > select');
-    await page.keyboard.type('d');
-    // await page.keyboard.press('ArrowDown'); // time of going JB 
-    // await page.keyboard.press('ArrowDown'); // 7am-12pm
-    // await page.keyboard.press('ArrowDown'); // 12pm-7pm
-    // await page.keyboard.press('ArrowDown'); // 7pm-12am
-    for (var i = 1; i <= timeReturn; i++) {
-        await page.keyboard.press('ArrowDown')
-    }
-    await page.keyboard.press('Escape');
+        await page.click('#taboneway > form > ul:nth-child(1) > li:nth-child(3) > div > input'); // select date
+        await page.click('#taboneway > form > ul:nth-child(1) > li:nth-child(3) > div > div > ul > li.uib-button-bar.ng-scope > span > button.btn.btn-sm.btn-danger.uib-clear.ng-binding');
+        await page.keyboard.type(date); 
 
-    await page.waitFor(1000); // check time
+        await page.click('#taboneway > form > ul:nth-child(1) > li:nth-child(4) > select'); // select timing
+        await page.keyboard.type('d');
+        for (var i = 1; i <= timeReturn; i++) {
+            await page.keyboard.press('ArrowDown')
+        }
+        await page.keyboard.press('Escape');
 
-    await page.click('#taboneway > form > ul:nth-child(3) > li:nth-child(1) > select');
-    await page.keyboard.type('2'); // 2 adults
-    await page.keyboard.press('Escape');
+        await page.waitFor(1000); // for user to do visual check time
 
-    await page.click('#taboneway > form > ul:nth-child(3) > li:nth-child(3) > input');
+        await page.click('#taboneway > form > ul:nth-child(3) > li:nth-child(1) > select');
+        await page.keyboard.type('2'); // 2 adults
+        await page.keyboard.press('Escape');
 
-    // await page.waitFor(15000);
-    // browser.close();
+        let picName = date.replace('/', '-'); // date string contains '/' which interferes with saving pic
+        picName = picName.replace('/', '-'); // do it twice since can't no replaceAll function and using escape // is a comment
+        await page.screenshot({ fullPage:true, path: `${picName}.png` }); // take a pic before sending request
+        await page.click('#taboneway > form > ul:nth-child(3) > li:nth-child(3) > input');
 
-};
+        await page.waitFor(5000);      
+        const result = await page.evaluate(() => { // check train timing results
+            
+            let initialRes = document.querySelector('#\\31  > div:nth-child(3) > div:nth-child(1) > div > span.skin-clr');            
+            if (initialRes == null) {
+                initialRes = document.querySelector('#tab4 > div:nth-child(4)');
+            }    
+            let text = initialRes.innerText;        
+            return text;
+        });
+        // console.log(result);
+        return result;
+        
+    };
+
+    for (var i = 0; i < dateReturnArray.length; i++) { // iterate through dates provided
+        let x = 0; // reinitialise it everytime.
+        x = await returnKTM(dateReturnArray[i]);
+        if (x.includes('Opps')) {
+            console.log(`No return train for ${dateReturnArray[i]}, pls try again.`);
+        } else { // else if x.includes(positive results)
+        console.log(`Success! Got return train for ${dateReturnArray[i]}, go book it`);
+        repeat = false; 
+        };
+    };
 
 
 // scrape().then((value) => {
